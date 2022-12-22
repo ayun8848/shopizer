ba3ebbd8b3aee8beb78ba4d55c12c51db798f08e
王小龙 <xiaolong.wang@chinaredstar.com>
2022-12-22 14:49:22 +0800

    add aws codedeploy files

---
+++ sm-shop/script/startup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+TOMCAT_HOME=""
+echo "scanning tomcat home im /usr"
+for path in `sudo find /usr -maxdepth 3 -type d -name \*tomcat\*`
+do
+        if [ -d "$path" ] && [ -e "${path}/bin/catalina.sh" ]; then
+            echo "found tomcat home: ${path}"
+            TOMCAT_HOME=$path
+            break
+        fi
+done
+
+if [ -z "$TOMCAT_HOME" ]; then
+        echo "tomcat home not found"
+else
+        echo "start tomcat..."
+        $TOMCAT_HOME/bin/startup.sh
+fi
