925487fd935aadab39e5d387b7262403723ce3f7
mathsengine <ayun8848@163.com>
2023-09-17 17:06:09 -0600

    get category by code

--- sm-shop/src/main/java/com/salesmanager/shop/store/api/v1/category/CategoryApi.java
+++ sm-shop/src/main/java/com/salesmanager/shop/store/api/v1/category/CategoryApi.java
@@ -101,6 +101,17 @@ public class CategoryApi {
 		return new ResponseEntity<EntityExists>(new EntityExists(isCategoryExist), HttpStatus.OK);
 	}
 
+	@ResponseStatus(HttpStatus.OK)
+	@GetMapping(value = { "/private/category/code" }, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ApiImplicitParams({ @ApiImplicitParam(name = "store", dataType = "string", defaultValue = "DEFAULT"),
+			@ApiImplicitParam(name = "lang", dataType = "string", defaultValue = "en") })
+	@ApiOperation(httpMethod = "GET", value = "Check if category code already exists", notes = "", response = EntityExists.class)
+	public ReadableCategory getIdByCode(@RequestParam(value = "code") String code,
+											   @ApiIgnore MerchantStore merchantStore, @ApiIgnore Language language) throws Exception {
+		ReadableCategory category = categoryFacade.getByCode(merchantStore, code, language);
+		return category;
+	}
+
 	/**
 	 * Get all category starting from root filter can be used for filtering on
 	 * fields only featured is supported
